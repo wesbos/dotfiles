01efcae6cc5db27104a8a88edcba8b569fbc012c
Wes Bos <wesbos@gmail.com>
2018-02-06 14:27:19 -0500

    vs code settings

---
+++ vscode.js
@@ -0,0 +1,62 @@
+{
+  "window.zoomLevel": -1,
+  "editor.fontFamily": "Operator Mono, Menlo, Monaco, 'Courier New', monospace",
+  "editor.fontSize": 29,
+  "editor.lineHeight": 40,
+  "editor.letterSpacing": 0.5,
+  "workbench.editor.tabSizing": "shrink",
+  "html.format.enable": true,
+  "html.format.preserveNewLines": true,
+  "files.trimTrailingWhitespace": true,
+  "editor.tabSize": 2,
+  // this isn't really underline but we hack it to be a thicker cursor
+  "editor.cursorStyle": "line",
+  "editor.wordWrap": "on",
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.fontWeight": "400",
+  "editor.cursorBlinking": "solid",
+  "editor.colorDecorators": false,
+  "editor.minimap.enabled": false,
+  "vscode_custom_css.imports": [
+    "/Volumes/Macintosh HD/Users/wesbos/.vscodestyles.css"
+  ],
+  "workbench.activityBar.visible": true,
+  "workbench.sideBar.location": "left",
+  "gitlens.blame.line.enabled": false,
+  "editor.renderWhitespace": "none",
+  "editor.rulers": [
+    80,
+    100,
+    120
+  ],
+  "editor.minimap.showSlider": "always",
+  "workbench.startupEditor": "newUntitledFile",
+  "extensions.ignoreRecommendations": false,
+  "files.exclude": {
+    "**/.git": true,
+    "**/.svn": true,
+    "**/.hg": true,
+    "**/CVS": true,
+    "**/.DS_Store": true,
+    "**/.next": true
+  },
+  "workbench.colorTheme": "Cobalt2",
+  "workbench.editor.tabCloseButton": "off",
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  // show snippets before any other auto completion
+  "editor.snippetSuggestions": "top",
+  // this lets you quick open multiple items in a row and now have the previous ones closed on you
+  "workbench.editor.enablePreviewFromQuickOpen": false,
+  "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
+  "sublimeTextKeymap.promptV3Features": true,
+  // These are all my auto-save configs
+  "editor.formatOnSave": true,
+  // turn it off for JS
+  "[javascript]": {
+    "editor.formatOnSave": false
+  },
+  "eslint.autoFixOnSave": true,
+  "eslint.alwaysShowStatus": true,
+}
