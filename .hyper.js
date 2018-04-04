acf7ecceaceb9a1874621d4caa92e03ca3c0918b
Wes Bos <wesbos@gmail.com>
2018-04-04 14:36:46 -0400

    HYPE

--- .hyper.js
+++ .hyper.js
@@ -5,59 +5,19 @@ module.exports = {
     fontSize: 20,
     windowSize: [1080, 720],
     fontFamily: '"Operator Mono", "Inconsolata for Powerline", monospace',
-    cursorColor: 'rgba(248,28,229,0.75)',
     cursorShape: 'BLOCK',
-    foregroundColor: '#fff',
-    backgroundColor: '#000',
-    borderColor: '#333',
-    css: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-
-    colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#46DD3C',
-      yellow: '#ffc600',
-      blue: '#1478DB',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
-    },
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
+    wickedBorder: true,
+    padding: '10px',
     shell: '/bin/zsh'
-
-    // for advanced config flags please refer to https://hyperterm.org/#cfg
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  // plugins: [
-  //   'hyperterm-cobalt2-theme',
-  // ],
+
+  plugins: [
+    'hyperterm-cobalt2-theme',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: ['hyperterm-cobalt2-theme'],
-
+  // localPlugins: ['hyperterm-cobalt2-theme'],
 };
